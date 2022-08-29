5d94a06a8f31993860d3d439123c6b83f2c4e3a7
Shaun Twiddy <107031443+stwiddy@users.noreply.github.com>
2022-08-29 06:01:10 -1000

    Morning_Algo_update

--- MERN_Stack/MorningAlgos/MorningAlgoW1D1.js
+++ MERN_Stack/MorningAlgos/MorningAlgoW1D1.js
@@ -1,10 +1,11 @@
-// Time Complexity
-// - Best: O(n) linear when array is already sorted.
-// - Average: O(n^2) quadratic.
-// - Worst: O(n^2) quadratic when the array is reverse sorted.
-// Space: O(1) constant.
-// For review, create a function that uses BubbleSort to sort an unsorted array in-place.
-// For every pair of adjacent indices, swap them if the item on the left is larger than the item on the right until array is fully sorted
+
+//   Time Complexity
+//     - Best: O(n) linear when array is already sorted.
+//     - Average: O(n^2) quadratic.
+//     - Worst: O(n^2) quadratic when the array is reverse sorted.
+//   Space: O(1) constant.
+//   For review, create a function that uses BubbleSort to sort an unsorted array in-place.
+//   For every pair of adjacent indices, swap them if the item on the left is larger than the item on the right until array is fully sorted
 // */
 
 const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -13,10 +14,29 @@ const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 /**
-* Sorts the given nums in-place by mutating the array.
-* Best: O(n) linear when array is already sorted.
-* Average: O(n^2) quadratic.
-* Worst: O(n^2) quadratic when the array is reverse sorted.
-* @param {Array<number>} nums
-* @returns {Array<number>} The given nums after being sorted.
-*/
+ * Sorts the given nums in-place by mutating the array.
+ * Best: O(n) linear when array is already sorted.
+ * Average: O(n^2) quadratic.
+ * Worst: O(n^2) quadratic when the array is reverse sorted.
+ * @param {Array<number>} nums
+ * @returns {Array<number>} The given nums after being sorted.
+ */
+function bubbleSort(nums = []) {
+  let pointer = nums.length-1
+  for(var i = 0;i < nums.length-1; i++){
+    for(var j = 0; j < pointer;j++){
+      var temp;
+      if (nums[j] > nums[j+1]){
+        temp = nums[j]
+        nums[j] =  nums[j+1]
+        nums[j+1] = temp
+      }
+    }
+    pointer--;
+  }
+  return nums
+}
+
+console.log(bubbleSort(numsRandomOrder))
+console.log(bubbleSort(numsReversed))
+console.log(bubbleSort(numsOrdered))
